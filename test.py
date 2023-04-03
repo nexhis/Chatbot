1f8bfc1005615ec6afca0902da06cf23413cc584
Jake Cross <jackgw@mail.umkc.edu>
2023-04-03 00:00:52 -0500

    Add stop words

--- test.py
+++ test.py
@@ -5,22 +5,22 @@ import gradio as gr
 #if you have OpenAI API key as a string, enable the below
 openai.api_key = "xxxxx"
 
-bot_name = "\nChatbot:"
-user_name = "User:"
-background = "User is talking to Chatbot, who is helpful, creative, and very friendly and knows everything. Chatbot is created by Group3."
+bot_name = "Chatbot"
+user_name = "User"
+background = "User is talking to Chatbot. Chatbot is helpful, creative, and very friendly and knows everything. Chatbot is created by Group3."
 
 
 def ask(question, chat_log=None):
-    prompt_text = f'{chat_log} {user_name} {question} {bot_name}'
+    prompt_text = f'{chat_log} {user_name}: {question} {bot_name}: '
     response = openai.Completion.create(
-      engine='text-ada-001',
+      engine='text-babbage-001',
       prompt=prompt_text,
       temperature=0.9,
       max_tokens=256,
       top_p=1,
       frequency_penalty=0,
-      presence_penalty=0.6,
-      stop=["\n"],
+      presence_penalty=0,
+      stop=["\n", "Chatbot:", "User:"],
     )
     story = response['choices'][0]['text']
     return str(story)
@@ -28,20 +28,19 @@ def ask(question, chat_log=None):
 
 # Function that clones the GPT-3 model for conversation
 
-
-
 def chatgpt_clone(input, history):
     log_list = history or []    # Initialize the chat history if it is None
     s = list(sum(log_list, ()))
-    chat_log = background + (' '.join(s))
-    answer = ask(input, chat_log)
+    
+    chat_log = background + (' '.join(s))   # Combine the backgorund info with the chat history
+    answer = ask(input, chat_log)   # Send the input and chat history to the GPT-3 model and get the response  
 
-    user_log = f"{user_name} {input}"
-    bot_log = f"{bot_name} {answer}"
+    # Add the current input and response to the chat history
+    user_log = f"{user_name}: {input}"
+    bot_log = f"{bot_name}: {answer}"
+    log_list.append((user_log, bot_log))
 
-    log_list.append((user_log, bot_log))   # Add the current input and response to the chat history
-
-    return log_list, log_list          # Return the updated chat history 
+    return log_list, log_list   # Return the updated chat history 
 
 
 
